6a0ea75f1bcd9c5cc2e080272985a510b273f9e0
Andy Nixon <andielane@yahoo.com>
2016-10-18 10:58:08 -0700

    Removing unnecessary code and making adjustments to database.

--- DBserver.js
+++ DBserver.js
@@ -21,57 +21,46 @@ mongoose.connect('mongodb://localhost/iatse', function(err, db){
   } else {
     console.log("Cobra vs mongoose")
   }
-}); // connect to mongo database named iatse 
+}); 
 
 var Schema = mongoose.Schema;
 
 var jobSchema = new Schema({
     year: {type: Number, required: true},
-    JobNumber: Number,
-    Employer: String,
-    PayrollCompany: String,
-    Show: String,
-    RTPerson: String,
-    Site: String,
-    RTLocation: String,
-    Day: String,
+    jobNumber: Number,
+    employer: String,
+    payroll: String,
+    show: String,
+    rtPerson: String,
+    site: String,
+    rtLocation: String,
+    day: String,
+    date: String,
     callBacks: String,
     position: String,
     dress: String,
     contract: String,
     rate: Number,
     steward: String,
-    paidInFull: Boolean,
-    PartiallyPaid: Boolean,
-    notPaid: Boolean,
+    paymentStatus: {
+      paidInFull: Boolean,
+      partiallyPaid: Boolean,
+      notPaid: Boolean
+    },
 });
 
 var Job = mongoose.model("Job", jobSchema);
 
-// app.use(express.static(path.join(__dirname, 'wageCalculator')));
-
 app.post('/iatse', function(req, res){
   res.send();
   var newJob = new Job();
-  
-  newJob.year = req.body.year;
-  newJob.JobNumber = req.body.jobNum;
-  newJob.Employer = req.body.employer;
-  newJob.PayrollCompany = req.body.payroll;
-  newJob.show = req.body.payroll;
-  newJob.RTPerson = req.body.rtPerson;
-  newJob.Site = req.body.site;
-  newJob.RTLocation = req.body.rtLocation;
-  newJob.Day = req.body.day;
-  newJob.callBacks = req.body.callBacks;
-  newJob.position = req.body.position;
-  newJob.dress = req.body.dress;
-  newJob.contract = req.body.contract;
-  newJob.rate = req.body.rate;
-  newJob.steward = req.body.steward;
-  newJob.paidInFull = req.body.paidInFull;
-  newJob.PartiallyPaid = req.body.partialPaid;
-  newJob.notPaid = req.body.notPaid;
+  for(var key in newJob) {
+    for(var key2 in req.body){
+      if(key === key2){
+        newJob[key] = req.body[key2]
+      }
+    };
+  };
 
   newJob.save(function(err){
     if(err){
@@ -82,7 +71,6 @@ app.post('/iatse', function(req, res){
   })
 });
 
-// export our app for testing and flexibility, required by index.js
 var port = 3000;
 app.listen(port, function(){
   console.log("server running on port ", port);
--- jobInput/jobInput.js
+++ jobInput/jobInput.js
@@ -5,13 +5,12 @@ class JobsInput extends React.Component {
 
 	inputSubmit(e) {
 		e.preventDefault();
-		console.log(this.paid.checked)
 		fetch('http://localhost:3000/iatse', {
 			method: 'POST',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
 				year: this.year.value,
-				jobNum: this.jobNum.value,
+				jobNumber: this.jobNumber.value,
 				employer: this.employer.value,
 				payroll: this.payroll.value,
 				show: this.show.value,
@@ -19,30 +18,28 @@ class JobsInput extends React.Component {
 				site: this.site.value,
 				rtLocation: this.rtLocation.value,
 				day: this.day.value,
+				date: this.date.value,
 				callBacks: this.callBacks.value,
 				position: this.position.value,
 				dress: this.dress.value,
 				contract: this.contract.value,
-				rate: this.rate.checked,
+				rate: this.rate.value,
 				steward: this.steward.value,
-				paidInFull: this.paid.checked,
-				partialPaid: this.partialPaid.checked,
-				notPaid: this.notPaid.checked,
+				paymentStatus: {
+						paidInFull: this.paidInFull.checked,
+						partiallyPaid: this.partiallyPaid.checked,
+						notPaid: this.notPaid.checked
+					},
 			})
-		}).then(res => console.log(res));
+		})
 	}
 
-	checkedOrNot() {
-		if(this.paid.checked){
-			this.partialPaid.checked === false;
-		}
-	}
 
 	render(){
 		return <div>
 				<form>
 				Year: <input type='text' ref={(year) => this.year = year}/><br/>
-				Job Number: <input type='text' ref={(jobNum) => this.jobNum = jobNum}/><br/>
+				Job Number: <input type='text' ref={(jobNumber) => this.jobNumber = jobNumber}/><br/>
 				Employer: <input type='text' ref={(employer) => this.employer = employer}/><br/>
 				Payroll Company: <input type='text' ref={(payroll) => this.payroll = payroll}/><br/>
 				Show: <input type="text" ref={(show) => this.show = show}/><br/>
@@ -50,14 +47,15 @@ class JobsInput extends React.Component {
 				Site: <input type="text" ref={(site) => this.site = site}/><br/>
 				RT Location: <input type="text" ref={(rtLocation) => this.rtLocation = rtLocation}/><br/>
 				Day: <input type="text" ref={(day) => this.day = day}/><br/>
+				Date: <input type="date" ref={(date) => this.date = date}/><br/>
 				Call Backs: <input type="text" ref={(callBacks) => this.callBacks = callBacks}/><br/>
 				Position: <input type="text" ref={(position) => this.position = position}/><br/>
 				Dress: <input type="text" ref={(dress) => this.dress = dress}/><br/>
 				Contract: <input type="text" ref={(contract) => this.contract = contract}/><br/>
 				Rate: <input type="text" ref={(rate) => this.rate = rate}/><br/>
 				Steward: <input type="text" ref={(steward) => this.steward = steward}/><br/>
-				Paid in Full: <input type="radio" name="PayStatus" ref={(paid) => this.paid = paid}/><br/>
-				Partially Paid: <input type="radio" name="PayStatus" ref={(partialPaid) => this.partialPaid = partialPaid}/><br/>
+				Paid in Full: <input type="radio" name="PayStatus" ref={(paid) => this.paidInFull = paid}/><br/>
+				Partially Paid: <input type="radio" name="PayStatus" ref={(partiallyPaid) => this.partiallyPaid = partiallyPaid}/><br/>
 				Not Paid: <input type='radio' name="PayStatus" ref={(notPaid) => this.notPaid = notPaid}/><br/>
 				<input type="submit" onClick={this.inputSubmit.bind(this)} value="Submit"/>
 				</form>
